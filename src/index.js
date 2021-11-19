36c5e3be0e1da7c2ebf792f940b4ed4cd4615c64
mellowmorgan <morgan.waites@gmail.com>
2021-11-19 11:55:53 -0800

    fix margin

--- src/index.js
+++ src/index.js
@@ -7,14 +7,17 @@ import CurrencyExchangerInterface from './js/currencyExchangerInterface.js';
 
 function populateMenus(response){
   if (response.conversion_rates) {
-    
     Object.keys(response.conversion_rates).forEach(function(currency) {
       $('#currencyFrom').append(`<option value="${currency}"> ${currency} </option>`);
-      if(currency==="CAD"){$('#currencyTo').append(`<option value="${currency}" selected> ${currency} </option>`);}
-      else{$('#currencyTo').append(`<option value="${currency}"> ${currency} </option>`);}
-      
+      if(currency==="CAD"){
+        $('#currencyTo').append(`<option value="${currency}" selected> ${currency} </option>`);
+      }
+      else{
+        $('#currencyTo').append(`<option value="${currency}"> ${currency} </option>`);
+      }
     });
-  } else {
+  } 
+  else {
     $('.showErrors').text(`There was an error: ${response.error}`);
   }
 }
@@ -60,7 +63,6 @@ async function makeAPICall(currFrom, currTo, amount){
 function clearVals(){
   $(".results").empty();
   $(".showErrors").empty();
-
 }
 
 $(document).ready(()=>{
