e50988d0f9e36f6343475bff26577bd94d1a4173
mellowmorgan <morgan.waites@gmail.com>
2021-11-19 08:34:22 -0800

    add currencyExchangerInterface class

--- src/index.js
+++ src/index.js
@@ -2,4 +2,12 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import currencyExchangerInterface from './js/currencyExchangerInterface.js';
 
+$(document).ready(()=>{
+
+
+  $("#form-converter").submit((event)=>{
+
+  });
+});
