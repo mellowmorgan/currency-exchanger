0b5ad6b26d16fca426fa3b64cf084d0cd0f64b2d
mellowmorgan <morgan.waites@gmail.com>
2021-11-19 08:50:35 -0800

    add populateMenus function

--- src/index.js
+++ src/index.js
@@ -4,13 +4,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import CurrencyExchangerInterface from './js/currencyExchangerInterface.js';
 
-function popylateMenus(currencies){
-  
+function populateMenus(response){
+  if (response.conversion_rates) {
+    Object.keys(response.conversion_rates).forEach(function(currency) {
+      $('#currencyFrom').append(`<option value="${currency}"> ${currency} </option>`);
+      $('#currencyTo').append(`<option value="${currency}"> ${currency} </option>`);
+    });
+  } else {
+    $('.showError').text(`There was an error: ${response}`);
+  }
 }
 
 async function makePopuplateCall(){
-  const currencies = await CurrencyExchangerInterface.getCurrencies();
-  populateMenus(currencies);
+  const response = await CurrencyExchangerInterface.getCurrencies();
+  populateMenus(response);
 
 }
 
