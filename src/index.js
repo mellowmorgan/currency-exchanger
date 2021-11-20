f6875261beec362bb7848fcf16acde9d2870a3a5
mellowmorgan <morgan.waites@gmail.com>
2021-11-19 16:27:04 -0800

    WIP: trying to do sessionStorage

--- src/index.js
+++ src/index.js
@@ -63,6 +63,20 @@ function clearVals(){
   $(".showErrors").empty();
 }
 
+//sessionStorage
+myStorage = window.sessionStorage;
+sessionStorage.setItem('key', 'value');
+
+// Get saved data from sessionStorage
+let data = sessionStorage.getItem('key');
+
+// Remove saved data from sessionStorage
+sessionStorage.removeItem('key');
+
+// Remove all saved data from sessionStorage
+sessionStorage.clear();
+
+
 $(document).ready(()=>{
   makePopuplateCall();
   $("#form-converter").submit((event)=>{
